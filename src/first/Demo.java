d49645901f111dc4f597238ac3f96b1b5573875b
raedu <raedu@raedu-ws.partnet.cisco.com>
2014-06-17 18:40:56 +0530

    4th commit

--- src/first/Demo.java
+++ src/first/Demo.java
@@ -3,7 +3,7 @@ package first;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		final String sudha="LALALA";
+		final String sudha="LALALA++";
 	}
 
 }
